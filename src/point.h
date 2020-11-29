c6a2b75355db2718f18970c890d431e40eb87d86
Artem MELNYK <melnyk.artem@qenvi.com>
2020-11-29 13:26:33 +0100

    classes constructors creation

--- src/point.h
+++ src/point.h
@@ -2,16 +2,24 @@
 #define POINT_H
 class Point
 {
- public:
- void setX(double x);
- void setY(double y);
- double getX();
- double getY();
- double distance(const Point &P);
- Point milieu(const Point &P);
- void saisir();
- void afficher();
- private:
- double x,y;
+    private:
+        double x,y;
+           
+    public:
+        // Default Constructors: Default constructor is the constructor which doesn’t take any argument. It has no parameters.
+        Point(){ x = 0.0; y = 0.0;}
+        // Parameterized Constructors: Passing the arguments to constructors to initialize an object when it is created.
+        Point(double x, double y){ this->x = x;  this->y = y;}
+        // Copy constructor 
+        Point(const Point &p) {this->x = p.x; this->y = p.y; } 
+        //
+        void setX(double x);
+        void setY(double y);
+        double getX();
+        double getY();
+        double distance(const Point &P);
+        Point milieu(const Point &P);
+        void saisir();
+        void afficher();
 };
 #endif
