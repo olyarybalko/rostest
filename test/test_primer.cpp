914ae25fb2fa209cc432616c5998ca9facbee1b8
Melnyk Olga <olyarybalko@gmail.com>
2020-12-01 20:58:30 +0100

    test fonction getFreeRam getUptimeSys getLoadAverage

--- test/test_primer.cpp
+++ test/test_primer.cpp
@@ -52,25 +52,121 @@ TEST(TestSuite, testDistance)
   ASSERT_DOUBLE_EQ(d, 2.8284);
 }
 
-//test Systeminfo: type string, longueur de retour <=128, time de reponce <= 6 ms 
+//test Systeminfo: type string, longueur de retour <=128, time de reponce <= 6 ms
+
+//test getSerialNumber: type string, longueur de retour 000000000000000d == 16, time de reponce <= 5 ms 
+//test getDateTime: type string, longueur de retour 2020-11-30.21:56:51 == 19, time de reponce <= 2 ms 
+
+//test getFreeRam type string, longueur de retour <=32, time de reponce <= 2 ms 
+//test getUptimeSys type string, longueur de retour <=32, time de reponce <= 2 ms 
+//test getLoadAverage type string, longueur de retour <=5, time de reponce <= 2 ms 
+
 TEST(TestSuite, testSysteminfo)
 {
   struct timespec now;
   double start, end;
 
   SystemInfo infotime;
-  std::string info;
-  EXPECT_TRUE(typeid(info) == typeid(infotime.getUpTime()));
+  EXPECT_TRUE(typeid(std::string) == typeid(infotime.getUpTime()));
 
   ASSERT_LE(infotime.getUpTime().size(), 128);
-
+  
+  // test time de  reponce
   clock_gettime(CLOCK_MONOTONIC, &now);
   start=now.tv_sec+1.e-9*now.tv_nsec;
   infotime.getUpTime();
   clock_gettime(CLOCK_MONOTONIC, &now);
   end=now.tv_sec+1.e-9*now.tv_nsec;
-  ASSERT_LE((end-start), .1);
+  ASSERT_LE((end-start), .06);
+} 
+
+TEST(TestSuite, testFreeRam)
+{
+  struct timespec now;
+  double start, end;
+
+  SystemInfo infoRam;
+  EXPECT_TRUE(typeid(std::string) == typeid(infoRam.getFreeRam()));
+
+  ASSERT_LE(infoRam.getFreeRam().size(), 32);
+
+  // test time de  reponce
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  start=now.tv_sec+1.e-9*now.tv_nsec;
+  infoRam.getFreeRam();
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  end=now.tv_sec+1.e-9*now.tv_nsec;
+  ASSERT_LE((end-start), .02);
 }
+
+TEST(TestSuite, testUptimeSys)
+{
+  struct timespec now;
+  double start, end;
+
+  SystemInfo infoUptime;
+  EXPECT_TRUE(typeid(std::string) == typeid(infoUptime.getUptimeSys()));
+
+  ASSERT_LE(infoUptime.getUptimeSys().size(), 32);
+
+  // test time de  reponce
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  start=now.tv_sec+1.e-9*now.tv_nsec;
+  infoUptime.getUptimeSys();
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  end=now.tv_sec+1.e-9*now.tv_nsec;
+  ASSERT_LE((end-start), .02);
+}
+
+TEST(TestSuite, testLoadAverage)
+{
+  struct timespec now;
+  double start, end;
+
+  SystemInfo infoLoadAverage;
+
+  // test switch case 1
+  EXPECT_TRUE(typeid(std::string) == typeid(infoLoadAverage.getLoadAverage(1)));
+
+  ASSERT_LE(infoLoadAverage.getLoadAverage(1).size(), 5);
+
+  long double LoadDouble_0 = std::stold(infoLoadAverage.getLoadAverage(1));
+  ASSERT_LE(0.0, LoadDouble_0);
+
+  // test switch case 0
+  EXPECT_TRUE(typeid(std::string) == typeid(infoLoadAverage.getLoadAverage(0)));
+
+  ASSERT_LE(infoLoadAverage.getLoadAverage(0).size(), 5);
+
+  long double LoadDouble_1 = std::stold(infoLoadAverage.getLoadAverage(0));
+  ASSERT_LE(0.0, LoadDouble_1);
+
+  // test time de  reponce
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  start=now.tv_sec+1.e-9*now.tv_nsec;
+  infoLoadAverage.getLoadAverage(1);
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  end=now.tv_sec+1.e-9*now.tv_nsec;
+  ASSERT_LE((end-start), .02);
+}
+
+/*TEST(TestSuite, testRam)
+{
+  struct timespec now;
+  double start, end;
+
+  SystemInfo infoRam;
+  EXPECT_TRUE(typeid(std::string) == typeid(infoRam.getRam()));
+
+  ASSERT_LE(infoRam.getRam().size(), 32);
+
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  start=now.tv_sec+1.e-9*now.tv_nsec;
+  infoRam.getRam();
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  end=now.tv_sec+1.e-9*now.tv_nsec;
+  ASSERT_LE((end-start), .02);
+}*/
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
